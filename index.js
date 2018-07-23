199c2e6f26fb68d9229e3334b527a1d5781e2f51
Lisa Mao <liisamao@gmail.com>
2018-07-23 19:33:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code in this file!
+function scuberGreetingForFeet (value) {
+  // scuberGreetingForFeet;
+  if (value<=400){
+    return`This one is on me!`
+  }else if(value>2000 && value<2499){
+    return `I will gladly take your thirty bucks.`;
+  }else if(value>2500){
+    return `No can do.`;
+  }
+}
+
+function ternaryCheckCity(city){
+  let ternaryCheckCity= (city=="NYC") ? "Ok, sounds good.":"No go.";
+  return (ternaryCheckCity);
+}
+
+function switchOnCharmFromTip(tip){
+  //let switchOnCharmFromTip;
+  switch(tip){
+    case 'generous':
+      return `Thank you so much.`;
+      break;
+    case 'not as generous':
+      return`Thank you.`;
+      break;
+    case 'thanks for everything':
+      return `Bye.`;
+      break;
+  }
+}
